cd8b28884d4fbf4616b8ffee4d2c9dc7c9c510b2
whdjh <wngns9807@gmail.com>
2025-10-12 19:23:35 +0900

    chore: 배경화면 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -63,7 +63,7 @@ export default function HomePage() {
 
   return (
     <div className="min-h-[calc(100dvh-4rem)] flex items-center justify-center px-4">
-      <div className="w-full max-w-sm rounded-2xl border border-white/10 backdrop-blur p-5 shadow-sm mb-40">
+      <div className="w-full max-w-sm rounded-2xl border border-white/10 backdrop-blur p-5 shadow-sm">
         <div className="flex flex-col gap-3">
           {/* 아이디 */}
           <div className="flex flex-col gap-1">
